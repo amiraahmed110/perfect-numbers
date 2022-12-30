646e81c679c4dcb5104258ed19f38219f90529df
amiraahmed110 <121686957+amiraahmed110@users.noreply.github.com>
2022-12-30 21:04:14 +0200

    Add files via upload

---
+++ Perfect Numbers/Perfect Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N1 , N2 ");
+            int N1 = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+            int i , s ;
+            for( int x =N1;x <=N2; x++)
+            { i = 1;
+                s = 0;
+            while(i < x )
+                {
+                    if (x % i == 0)
+                        s += i;
+                    i++;
+                }
+                if (s == x)
+                    Console.WriteLine("{0}" ,x);
+            
+            
+            }
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
